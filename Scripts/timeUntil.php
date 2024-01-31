ac13c8dc1bc3cbba583293ec01bf43cfafe2d3c5
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2024-01-31 13:10:00 +0000

    Create timeUntil.php

---
+++ Scripts/timeUntil.php
@@ -0,0 +1,45 @@
+<?php
+  // ini_set('display_errors', 1);
+  // ini_set('display_startup_errors', 1);
+  // error_reporting(E_ALL);
+  
+  $day = $_GET['day'] ?? "Sunday";
+  $time = $_GET['time'] ?? "6";
+  $ampm = $_GET['ampm'] ?? "pm";
+  $timez = $_GET['timezone'] ?? "Australia/Sydney";
+  
+  $day = strtolower($day);
+  $day = ucfirst($day);
+  
+  $time = intval($time);
+  
+  $timezone = new DateTimeZone($timez);
+  
+  $currentDateTime = new DateTime('now', $timezone);
+  $currentTime = $currentDateTime->format('H:i');
+  
+  if ($currentDateTime->format('l') == $day) {
+  	$nextDateTime = new DateTime('now', $timezone);
+  } else {
+  	$nextDateTime = new DateTime('next ' . $day, $timezone);
+  }
+  
+  $nextDateTime->setTime($time, 0);
+  
+  $interval = $currentDateTime->diff($nextDateTime);
+  
+  $days = $interval->d;
+  $hours = $interval->h;
+  $minutes = $interval->i;
+  $seconds = $interval->s;
+  
+  $result = '';
+  
+  if ($days > 0) $result .= $days . 'd, ';
+  if ($days > 0 || $hours > 0) $result .= $hours . 'h, ';
+  if ($minutes > 0) $result .= $minutes . 'm, ';
+  
+  $result .= $seconds . 's';
+  
+  echo rtrim($result, ', ');
+?>
