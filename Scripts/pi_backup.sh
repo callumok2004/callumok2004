b3e9ea5c1773ba42c421a309869f859e503fb6ba
Callum <35779365+callumok2004@users.noreply.github.com>
2025-07-22 00:45:43 +0100

    Update pi_backup.sh

--- Scripts/pi_backup.sh
+++ Scripts/pi_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BACKUP_SRC=("/root" "/var/www")
+BACKUP_SRC=("/root" "/var/www" "/var/lib/docker/volumes")
 MOUNT_POINT="/root/usb_share"
 BACKUP_DIR="$MOUNT_POINT/backup_$(date +%Y%m%d)"
 COMPRESS_DIR="$MOUNT_POINT/archives"
@@ -31,6 +31,7 @@ for SRC in "${BACKUP_SRC[@]}"; do
             tar -czf "$ARCHIVE_FILE" -C "$BACKUP_DIR" "$(basename $SRC)" >> "$LOG_FILE" 2>&1
             if [ $? -eq 0 ]; then
                 log "Compression of $SRC to $ARCHIVE_FILE completed successfully"
+                # Remove uncompressed backup to save space
                 rm -rf "$BACKUP_DIR/$(basename $SRC)"
                 log "Removed uncompressed backup for $SRC"
             else
@@ -53,4 +54,32 @@ find "$MOUNT_POINT" -type d -name "backup_*" -empty -delete -print | while read 
     log "Deleted empty backup directory: $dir"
 done
 
-log "Backup script completed"
+log "OS Backup script completed"
+log "HA Backup starting"
+
+CONTAINER_NAME="homeassistant"
+SOURCE_PATH="/config/backups"
+DEST_PATH="/root/usb_share/ha_backups"
+TMP_BACKUP_DIR="/tmp/ha_backups"
+
+mkdir -p "$TMP_BACKUP_DIR"
+mkdir -p "$DEST_PATH"
+
+find "$DEST_PATH" -type f -name '*.tar' -mtime +7 -delete
+
+docker exec "$CONTAINER_NAME" ls "$SOURCE_PATH" | grep '\.tar$' | while read backup_file; do
+  base_name="${backup_file%.tar}"
+
+  if ls "$DEST_PATH/${base_name}_copied_"*.tar &> /dev/null; then
+    continue
+  fi
+
+  docker cp "$CONTAINER_NAME:$SOURCE_PATH/$backup_file" "$TMP_BACKUP_DIR/$backup_file"
+
+  ts=$(date +%Y-%m-%d_%H-%M-%S)
+  cp "$TMP_BACKUP_DIR/$backup_file" "$DEST_PATH/${base_name}_copied_$ts.tar"
+done
+
+rm -rf "$TMP_BACKUP_DIR"
+
+log "Finished HA backup"
