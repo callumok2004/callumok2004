42714a259f038c3aba188c958567f550206d53d1
Callum <35779365+callumok2004@users.noreply.github.com>
2025-11-24 03:12:52 +0000

    Add optional ports property to Service interface

--- Scripts/services.ts
+++ Scripts/services.ts
@@ -6,8 +6,9 @@ const ServicesConfig = 'services.json'
 const ServicesRoot = './services/'
 
 interface Service {
-	name: string
-	command: string
+  name: string
+  command: string
+  ports?: string[]
 }
 
 const args = process.argv.slice(2)
@@ -18,70 +19,77 @@ const Config = JSON.parse(fs.readFileSync(ServicesConfig, 'utf8'))
 const Services = Config as Service[]
 
 function fileExists(filePath: string) {
-	try {
-		return fs.existsSync(filePath)
-	} catch {
-		return false
-	}
+  try {
+    return fs.existsSync(filePath)
+  } catch {
+    return false
+  }
+}
+
+function portArgs(service: Service) {
+  if (!service.ports || !service.ports.length) return ''
+  return service.ports.map(p => `-p ${p}`).join(' ')
 }
 
 function StartDockerContainer(service: Service) {
-	const name = `service-${service.name}`
-	const nameLower = name.toLowerCase()
-	const workdir = path.resolve(`${ServicesRoot}${service.name}`)
-	const dockerfilePath = path.join(workdir, 'Dockerfile')
+  const name = `service-${service.name}`
+  const nameLower = name.toLowerCase()
+  const workdir = path.resolve(`${ServicesRoot}${service.name}`)
+  const dockerfilePath = path.join(workdir, 'Dockerfile')
 
-	let command = ''
-	if (fileExists(dockerfilePath)) {
-		command = `docker build -t ${nameLower}-image ${workdir} && docker run -d --name ${name} ${nameLower}-image ${service.command}`
-	} else {
-		command = `docker run -d --rm --name ${name} -v ${workdir}:/app -w /app ${service.command}`
-	}
+  const ports = portArgs(service)
 
-	console.log(`Starting container for ${service.name} with command: ${service.command}`)
+  let command = ''
+  if (fileExists(dockerfilePath)) {
+    command = `docker build -t ${nameLower}-image ${workdir} && docker run -d --name ${name} ${ports} ${nameLower}-image ${service.command}`
+  } else {
+    command = `docker run -d --rm --name ${name} ${ports} -v ${workdir}:/app -w /app ${service.command}`
+  }
 
-	return new Promise((resolve, reject) => {
-		exec(command, (err, stdout, stderr) => {
-			if (err) {
-				console.error(`Error starting container for ${service.name}: ${err}`)
-				reject(err)
-			} else {
-				console.log(`Started container for ${service.name}`)
-				resolve(stdout)
-			}
-		})
-	})
+  console.log(`Starting container for ${service.name} with command: ${service.command}`)
+
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error starting container for ${service.name}: ${err}`)
+        reject(err)
+      } else {
+        console.log(`Started container for ${service.name}`)
+        resolve(stdout)
+      }
+    })
+  })
 }
 
 function RemoveServiceContainer(serviceName: string) {
-	const name = `service-${serviceName}`
+  const name = `service-${serviceName}`
 
-	console.log(`Removing container ${name}`)
+  console.log(`Removing container ${name}`)
 
-	const command = `docker rm -f ${name}`
+  const command = `docker rm -f ${name}`
 
-	return new Promise((resolve, reject) => {
-		exec(command, (err, stdout, stderr) => {
-			if (err) {
-				console.error(`Error removing container ${name}: ${err}`)
-				resolve(null)
-			} else {
-				console.log(`Removed container ${name}`)
-				resolve(stdout)
-			}
-		})
-	})
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error removing container ${name}: ${err}`)
+        resolve(null)
+      } else {
+        console.log(`Removed container ${name}`)
+        resolve(stdout)
+      }
+    })
+  })
 }
 
 if (selectedServiceName) {
-	const service = Services.find(s => s.name === selectedServiceName)
-	if (!service) {
-		console.error(`Service "${selectedServiceName}" not found in ${ServicesConfig}`)
-		process.exit(1)
-	}
-	await RemoveServiceContainer(service.name)
-	await StartDockerContainer(service)
+  const service = Services.find(s => s.name === selectedServiceName)
+  if (!service) {
+    console.error(`Service "${selectedServiceName}" not found in ${ServicesConfig}`)
+    process.exit(1)
+  }
+  await RemoveServiceContainer(service.name)
+  await StartDockerContainer(service)
 } else {
-	await Promise.all(Services.map(s => RemoveServiceContainer(s.name)))
-	await Promise.all(Services.map(StartDockerContainer))
+  await Promise.all(Services.map(s => RemoveServiceContainer(s.name)))
+  await Promise.all(Services.map(StartDockerContainer))
 }
